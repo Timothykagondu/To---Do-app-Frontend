53c9d07cb16a43046152a6b5f0591e8de5fadd2c
TimothyKagondu <timothy.2k21@gmail.com>
2023-03-27 10:39:15 +0300

    completed component homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,12 +3,54 @@ import {useState} from 'react'
 
 function Portfolio() {
 
+    const [inputValue, setInputValue] = useState('');
+    const [listItems, setListItems] = useState([]);
 
+    const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+    };
 
+    const handleAddItem = () => {
+        setListItems([...listItems, inputValue]);
+        setInputValue('');
+      };
+      return (
+        <div className="portfolio">
+        <img className="icon" src="https://img.icons8.com/bubbles/256/resume.png" alt="List"></img> 
 
+    <div className="proj1">
+        <h2>
+            View and Add Your To-Do list here :
+        </h2>
 
+        <h3>
+            Here you can add your To-Do
+        </h3>
+<br />
+<br />
+       
+        <div>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button className="btn1" onClick={handleAddItem}> Add Your Project </button>
 
-    
+<br />
+<br />
+<br />
+<br />
+
+      <ul className="list1">
+        {listItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+    <div>
+    </div>
+    </div>
+</div>  
+
+    );
+
 }
   
 export default Portfolio;
